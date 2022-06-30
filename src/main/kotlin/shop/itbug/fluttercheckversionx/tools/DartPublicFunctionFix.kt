f893ab1503a817b68a28456ae3dd35feadd08a39
mddd <413153189@qq.com>
2022-06-30 11:35:49 +0800

    新增自动修复功能

--- src/main/kotlin/shop/itbug/fluttercheckversionx/tools/DartPublicFunctionFix.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/tools/DartPublicFunctionFix.kt
@@ -7,18 +7,18 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementFactory
+import com.intellij.psi.PsiElementFinder
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiFile
-import com.intellij.psi.tree.IElementType
+import com.intellij.psi.search.PsiShortNamesCache
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.util.PsiElementFilter
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.childrenOfType
-import com.intellij.psi.util.elementType
-import com.jetbrains.lang.dart.DartElementType
-import com.jetbrains.lang.dart.DartLanguage
-import com.jetbrains.lang.dart.psi.DartType
+import com.jetbrains.lang.dart.DartTokenTypes
+import com.jetbrains.lang.dart.psi.DartCallExpression
+import com.jetbrains.lang.dart.psi.DartVisitor
 import com.jetbrains.lang.dart.psi.impl.*
-import com.jetbrains.lang.dart.util.DartPsiImplUtil
-import com.jetbrains.lang.dart.util.DartResolveUtil
-import io.flutter.dart.DartPsiUtil
 
 
 val checkSuperClassNames = listOf<String>("StatefulWidget")
@@ -38,33 +38,60 @@ class DartPublicFunctionFix : LocalInspectionTool() {
 
 class DartPublicFunctionApiFixVisitor(val holder: ProblemsHolder) : PsiElementVisitor() {
 
+    fun getMethodCallRefre(dmdElement: DartMethodDeclarationImpl): PsiElement? {
+        ///获取返回值
+        val dartFuntionBodyList = dmdElement.childrenOfType<DartFunctionBodyImpl>()
+        if (dartFuntionBodyList.isNotEmpty()) {
+            val dartCallExpressionList = dartFuntionBodyList.first().childrenOfType<DartCallExpressionImpl>()
+            if (dartCallExpressionList.isNotEmpty()) {
+                val dartReferenceExpessionList =
+                    dartCallExpressionList.first().childrenOfType<DartReferenceExpressionImpl>()
+                if (dartReferenceExpessionList.isNotEmpty()) {
+                    return dartReferenceExpessionList.first()
+                }
+            }
+        }
+        return null
+    }
+
     override fun visitElement(element: PsiElement) {
         //如果节点是一个类
-        if(element is DartClassDefinitionImpl ) {
+        if (element is DartClassDefinitionImpl) {
 
             //判断是不是继承自StatefulWidget类
-           val superClassList = element.childrenOfType<DartSuperclassImpl>()
-            if(superClassList.isNotEmpty()){
+            val superClassList = element.childrenOfType<DartSuperclassImpl>()
+            if (superClassList.isNotEmpty()) {
                 val superClassName = superClassList.first().childrenOfType<DartTypeImpl>().first().text
-                if(checkSuperClassNames.contains(superClassName)){
+                if (checkSuperClassNames.contains(superClassName)) {
                     ///包含需要待检测的类
-                   val methods = element.childrenOfType<DartClassBodyImpl>().first().childrenOfType<DartClassMembersImpl>()
-                       .first().childrenOfType<DartMethodDeclarationImpl>()
-                   val names = methods.filter { it.childrenOfType<DartComponentNameImpl>().first().text == "createState" }
-                    if(names.isNotEmpty()){
+                    val methods =
+                        element.childrenOfType<DartClassBodyImpl>().first().childrenOfType<DartClassMembersImpl>()
+                            .first().childrenOfType<DartMethodDeclarationImpl>()
+                    val names =
+                        methods.filter { it.childrenOfType<DartComponentNameImpl>().first().text == "createState" }
+                    if (names.isNotEmpty()) {
                         val dmdElement = names.first() //DartMethodDeclarationImpl节点
-                        val name =  dmdElement.childrenOfType<DartComponentNameImpl>().first().text
-                        if(name.equals("createState")){
-                           val returnTypes = dmdElement.childrenOfType<DartReturnTypeImpl>()
-                            if(returnTypes.isNotEmpty()){
+                        val name = dmdElement.childrenOfType<DartComponentNameImpl>().first().text
+                        if (name.equals("createState")) {
+                            val returnTypes = dmdElement.childrenOfType<DartReturnTypeImpl>()
+                            if (returnTypes.isNotEmpty()) {
                                 val returnTypeEle = returnTypes.first()
-                                if(returnTypeEle.text.indexOf("_") == 0){
-                                    holder.registerProblem(returnTypes.first(),"dart建议将返回类型修改为公有变量. QQ自学群:667186542欢迎加入",
-                                    ProblemHighlightType.WARNING,PublicApiRenameFix(returnTypeEle,returnTypeEle.text))
+
+                                val returnEx = getMethodCallRefre(dmdElement)
+
+                                if (returnTypeEle.text.indexOf("_") == 0) {
+                                    holder.registerProblem(
+                                        returnTypes.first(),
+                                        "dart建议将返回类型修改为公有变量. Flutter自学群:667186542",
+                                        ProblemHighlightType.WARNING,
+                                        PublicApiRenameFix(returnTypeEle, returnTypeEle.text, dmdElement)
+                                    )
                                 }
 
                             }
                         }
+
+
                     }
                 }
             }
@@ -74,25 +101,62 @@ class DartPublicFunctionApiFixVisitor(val holder: ProblemsHolder) : PsiElementVi
 }
 
 ///修复类
-class PublicApiRenameFix(val element: PsiElement,var className: String): LocalQuickFixOnPsiElement(element) {
+class PublicApiRenameFix(val element: PsiElement, var className: String, var functionElement: PsiElement) :
+    LocalQuickFixOnPsiElement(element) {
     private val renameText = className.removePrefix("_")
 
     override fun getFamilyName(): String {
-        return "梁典典: 将它重命名为$renameText"
+        return "重命名为$renameText"
     }
+
     override fun getText(): String {
         return familyName
     }
 
     override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
 
-       val factory = PsiElementFactory.getInstance(project)
-       val newNamePsi = factory.createDummyHolder(renameText, IElementType(
-            "text",DartLanguage.INSTANCE
-        ),null)
+        val factory = PsiElementFactory.getInstance(project)
 
-
+        val newNamePsi = factory.createDummyHolder(renameText, DartTokenTypes.RETURN_TYPE, null)
         startElement.replace(newNamePsi)
+
+
+        val find = PsiTreeUtil.collectElements(functionElement, object : PsiElementFilter {
+            override fun isAccepted(element: PsiElement): Boolean {
+                if (element is DartReferenceExpressionImpl && element.text.equals(className)) {
+                    return true
+                }
+                return false
+            }
+        })
+
+        if (find.isNotEmpty()) {
+            for (i in find) {
+                val newEl = factory.createDummyHolder(renameText, DartTokenTypes.REFERENCE_EXPRESSION, null)
+                i.replace(newEl)
+            }
+        }
+
+
+        val classfind = PsiTreeUtil.collectElementsOfType(file.originalElement,DartComponentNameImpl::class.java)
+        println(classfind.size)
+        for(c in classfind){
+            if(c.text.equals(className)){
+                val newEl = factory.createDummyHolder(renameText, DartTokenTypes.COMPONENT_NAME, null)
+                c.replace(newEl)
+            }
+        }
+
     }
 
+
+}
+
+
+inline fun <reified T : PsiElement> findElementWithType(element: PsiElement): T? {
+    val childrens = element.childrenOfType<T>();
+    if (childrens.isNotEmpty()) {
+        return childrens.first()
+    }
+    return null
 }
