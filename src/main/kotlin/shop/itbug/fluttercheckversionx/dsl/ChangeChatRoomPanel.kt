db0e2382effcdcf044edf900fa1405c7cd54f0cb
梁典典 <413153189@qq.com>
2022-12-07 15:14:19 +0800

    优化

--- src/main/kotlin/shop/itbug/fluttercheckversionx/dsl/ChangeChatRoomPanel.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/dsl/ChangeChatRoomPanel.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.observable.properties.ObservableMutableProperty
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.dsl.builder.panel
-import shop.itbug.fluttercheckversionx.model.ChatModel
+import shop.itbug.fluttercheckversionx.i18n.PluginBundle
 import shop.itbug.fluttercheckversionx.socket.service.AppService
 
 ///切换房间的面板
@@ -15,7 +15,7 @@ fun changeRoomPanel () : DialogPanel {
     val names = rooms.stream().map { it.name }.toList()
     val pane = panel {
         row {
-            label("切换群聊")
+            label(PluginBundle.get("switch.rooms"))
         }
         row  {
             segmentedButton(names) { it }.bind(object : ObservableMutableProperty<String> {
@@ -32,12 +32,14 @@ fun changeRoomPanel () : DialogPanel {
                 override fun get(): String {
                     return curr?.name ?: ""
                 }
-
             })
 
+        }.visible(names.isNotEmpty())
+        row {
+            label(PluginBundle.get("empty")).visible(names.isEmpty())
         }
         row {
-            checkBox("设置为默认")
+            checkBox(PluginBundle.get("set.default"))
         }
     }
     return pane.addBorder()
--- src/main/kotlin/shop/itbug/fluttercheckversionx/dsl/DocPanel.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/dsl/DocPanel.kt
@@ -6,11 +6,12 @@ import com.intellij.ui.dsl.builder.panel
 import shop.itbug.fluttercheckversionx.document.Helper
 import shop.itbug.fluttercheckversionx.document.MyMarkdownDocRenderObject
 
+///markdown渲染弹窗
 fun docPanel(markdownText:String,project: Project) : DialogPanel {
     val p = panel {
         row {
             label("<html>${Helper.markdown2Html(MyMarkdownDocRenderObject(markdownText,project))}</html>")
         }
     }
-    return  p
+    return  p.addBorder()
 }
