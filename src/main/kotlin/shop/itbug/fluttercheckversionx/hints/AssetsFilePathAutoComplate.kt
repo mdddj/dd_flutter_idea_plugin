db0bd9adb104e3773359df9f00901aea25e01843
梁典典 <413153189@qq.com>
2022-12-29 09:59:00 +0800

    :fire: 添加智能补全资源路径功能

--- src/main/kotlin/shop/itbug/fluttercheckversionx/hints/AssetsFilePathAutoComplate.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/hints/AssetsFilePathAutoComplate.kt
@@ -3,9 +3,10 @@ package shop.itbug.fluttercheckversionx.hints
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
-import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
+import shop.itbug.fluttercheckversionx.icons.MyIcons
 
 /**
  * 资源文件路径自动补全
@@ -14,28 +15,23 @@ class AssetsFilePathAutoComplete : CompletionContributor(){
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
         val path = parameters.editor.project?.basePath
         val assetsPath = "$path/assets"
-        val findFileByUrl = VirtualFileManager.getInstance().findFileByUrl(assetsPath)
+        val findFileByUrl = LocalFileSystem.getInstance().findFileByPath(assetsPath)
         findFileByUrl?.apply {
             elementHandle(this,result)
         }.takeIf { findFileByUrl?.isDirectory == true }
-
         super.fillCompletionVariants(parameters, result)
     }
 
     private fun elementHandle(file: VirtualFile, result: CompletionResultSet) {
-        file.children.forEach { f ->
+        val cs = file.children.toList()
+        cs.forEach { f ->
             if(f.isDirectory){
                 elementHandle(f,result)
             }else{
-                result.addElement(MyPathElement(file))
+                val indexOf = f.path.indexOf("assets")
+                val withIcon = LookupElementBuilder.create(f.path.substring(indexOf)).withIcon(MyIcons.imageIcon)
+                result.addElement(withIcon)
             }
         }
     }
-}
-
-class MyPathElement(val file: VirtualFile):LookupElement() {
-    override fun getLookupString(): String {
-        return file.name
-    }
-
 }
--- src/main/kotlin/shop/itbug/fluttercheckversionx/icons/MyIcons.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/icons/MyIcons.kt
@@ -5,7 +5,7 @@ import com.intellij.util.ImageLoader
 
 object MyIcons {
     var dartPackageIcon = IconLoader.getIcon("/icons/dart.svg", MyIcons::class.java)
-
+    var imageIcon = IconLoader.getIcon("/icons/image.svg", MyIcons::class.java)
     var dartPluginIcon = IconLoader.getIcon("/icons/pluginIcon.svg", MyIcons::class.java)
 
     var dsShange = ImageLoader.loadFromResource("/images/dashange.jpeg")
