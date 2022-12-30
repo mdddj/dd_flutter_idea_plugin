6caa6600f32d38fc9955f0ca4009155f298f8ff9
梁典典 <413153189@qq.com>
2022-12-30 13:42:15 +0800

    :fire: 新增生产文档注释的操作

--- src/main/kotlin/shop/itbug/fluttercheckversionx/actions/GenerateFunctionDocument.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/actions/GenerateFunctionDocument.kt
@@ -3,29 +3,19 @@ package shop.itbug.fluttercheckversionx.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFileFactory
-import com.intellij.psi.PsiManager
-import com.intellij.psi.util.PsiElementFilter
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.lang.dart.psi.DartPsiCompositeElement
-import com.jetbrains.lang.dart.psi.impl.DartBlockImpl
 import com.jetbrains.lang.dart.psi.impl.DartComponentNameImpl
 import com.jetbrains.lang.dart.psi.impl.DartFormalParameterListImpl
 import com.jetbrains.lang.dart.psi.impl.DartNormalFormalParameterImpl
-import com.jetbrains.lang.dart.psi.impl.DartPsiCompositeElementImpl
-import com.jetbrains.lang.dart.util.DartElementGenerator
-import com.jetbrains.lang.dart.util.DartPsiImplUtil
-import io.flutter.dart.DartPsiUtil
-import org.intellij.plugins.markdown.lang.psi.MarkdownPsiElementFactory
 
+///生成方法的函数文档操作
 class GenerateFunctionDocument : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val data = e.getData(CommonDataKeys.PSI_ELEMENT)
         if (data is DartComponentNameImpl) {
             val nextSibling = data.nextSibling
             if (nextSibling is DartFormalParameterListImpl) {
-               val psis = PsiTreeUtil.collectElements(
+                val psis = PsiTreeUtil.collectElements(
                     nextSibling
                 ) { element -> element is DartNormalFormalParameterImpl }
                 val sb = StringBuilder()
