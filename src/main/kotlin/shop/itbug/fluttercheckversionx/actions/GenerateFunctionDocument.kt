0e99a7cb8386d253d9c9facecaa8499131b53b15
梁典典 <413153189@qq.com>
2022-12-31 09:54:43 +0800

    :fire: 新增生产文档注释的操作

--- src/main/kotlin/shop/itbug/fluttercheckversionx/actions/GenerateFunctionDocument.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/actions/GenerateFunctionDocument.kt
@@ -3,12 +3,13 @@ package shop.itbug.fluttercheckversionx.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
-import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElement
-import com.intellij.psi.impl.source.tree.PsiCommentImpl
+import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.lang.dart.DartTokenTypes
+import com.jetbrains.lang.dart.DartLanguage
 import com.jetbrains.lang.dart.psi.impl.DartComponentNameImpl
 import com.jetbrains.lang.dart.psi.impl.DartFormalParameterListImpl
 import com.jetbrains.lang.dart.psi.impl.DartNormalFormalParameterImpl
@@ -36,8 +37,12 @@ class GenerateFunctionDocument : AnAction() {
         }
     }
 
-    /// TODO Automatically generate document comments
     private fun createDartDocPsiElement(project: Project, element: PsiElement, text: String) {
         println("进来了.")
+        val psifile = PsiFileFactory.getInstance(project).createFileFromText(DartLanguage.INSTANCE, text)
+        WriteCommandAction.runWriteCommandAction(project) {
+            element.addBefore(psifile.navigationElement,element)
+        }
+
     }
 }
