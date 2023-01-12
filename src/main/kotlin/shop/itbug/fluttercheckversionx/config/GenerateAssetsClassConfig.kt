eb0e5c1aa3128cb9204aa3e036ed8d4955fdcaa0
梁典典 <413153189@qq.com>
2023-01-12 11:12:05 +0800

    :white_check_mark: 细节优化

---
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/config/GenerateAssetsClassConfig.kt
@@ -0,0 +1,39 @@
+package shop.itbug.fluttercheckversionx.config
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
+
+
+data class GenerateAssetsClassConfigModel(
+    //类型
+    var className: String = "R",
+    //文件名
+    var fileName: String = "R",
+    //保持路径
+    var path: String = "lib",
+    //不再提醒
+    var dontTip: Boolean = false,
+    //是否监听自动提醒
+    var autoListenFileChange: Boolean = false
+)
+
+@State(name = "DDGenerateAssetsClassConfig", storages = [Storage("DDGenerateAssetsClassConfig.xml")])
+class GenerateAssetsClassConfig private constructor() : PersistentStateComponent<GenerateAssetsClassConfigModel> {
+    private var model = GenerateAssetsClassConfigModel()
+    override fun getState(): GenerateAssetsClassConfigModel {
+        return model
+    }
+
+    override fun loadState(state: GenerateAssetsClassConfigModel) {
+        model = state
+    }
+
+
+    companion object {
+        fun getInstance(): GenerateAssetsClassConfig {
+            return service()
+        }
+    }
+}
--- src/main/kotlin/shop/itbug/fluttercheckversionx/dialog/AssetsAutoGenerateClassActionConfigDialog.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/dialog/AssetsAutoGenerateClassActionConfigDialog.kt
@@ -1,54 +1,63 @@
 package shop.itbug.fluttercheckversionx.dialog
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.dsl.builder.Align
-import com.intellij.ui.dsl.builder.BottomGap
-import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.*
+import shop.itbug.fluttercheckversionx.config.GenerateAssetsClassConfig
+import shop.itbug.fluttercheckversionx.config.GenerateAssetsClassConfigModel
 import javax.swing.Action
 import javax.swing.JComponent
 
 //生成资产文件的配置弹窗
 class AssetsAutoGenerateClassActionConfigDialog(project: Project) : DialogWrapper(project) {
 
+    private val configModel: GenerateAssetsClassConfigModel = GenerateAssetsClassConfig.getInstance().state
+
     init {
         super.init()
         title = "生成资产文件类"
         isResizable = false
-
     }
 
     override fun createCenterPanel(): JComponent {
-        return panel {
-            row("类    名") {
-                textField()
-            }
-            row ("文件名") {
-                textField()
-            }
-            row("目    录") {
-                textField()
-            }
-            row {
-                checkBox("保存且不再提醒")
-            }.contextHelp("可在设置中再次配置")
-            row {
-                checkBox("监听文件变化自动更新")
-            }.bottomGap(BottomGap.SMALL)
-            row {
-                button("生成"){
-
-                }.align(Align.FILL)
-            }
-            row {
-                comment("有任何问题请<a href='https://github.com/mdddj/dd_flutter_idea_plugin/issues'>提交bug</a>")
-            }
-        }
+        return ui
     }
+
+    val ui: DialogPanel
+        get() : DialogPanel {
+            val p : DialogPanel = panel {
+                row("类    名") {
+                    textField().bindText(configModel::className)
+                }
+                row("文件名") {
+                    textField().bindText(configModel::fileName)
+                }
+                row("目    录") {
+                    textField().bindText(configModel::path)
+                }
+                row {
+                    checkBox("保存且不再提醒").bindSelected(configModel::dontTip)
+                }.contextHelp("可在设置中再次配置")
+                row {
+                    checkBox("监听文件变化自动更新").bindSelected(configModel::autoListenFileChange)
+                }.bottomGap(BottomGap.SMALL)
+                row {
+                    button("生成") {
+                        ui.apply()
+                        println(configModel)
+                        GenerateAssetsClassConfig.getInstance().loadState(configModel)
+                    }.align(Align.FILL)
+                }
+                row {
+                    comment("有任何问题请<a href='https://github.com/mdddj/dd_flutter_idea_plugin/issues'>提交bug</a>")
+                }
+            }
+            return p
+        }
 
     override fun createActions(): Array<Action> {
         return emptyArray()
     }
-
 }
 
