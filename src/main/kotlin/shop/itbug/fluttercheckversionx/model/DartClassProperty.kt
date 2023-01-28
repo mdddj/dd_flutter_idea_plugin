0bef739f377c607c684b4c712e47377952f6d3fd
梁典典 <413153189@qq.com>
2023-01-28 17:36:49 +0800

    :rocket: 新增模型转freezed工具

--- src/main/kotlin/shop/itbug/fluttercheckversionx/model/DartClassProperty.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/model/DartClassProperty.kt
@@ -3,7 +3,18 @@ package shop.itbug.fluttercheckversionx.model
 import com.jetbrains.lang.dart.psi.impl.DartVarDeclarationListImpl
 import shop.itbug.fluttercheckversionx.util.DartPsiElementUtil
 
-fun DartVarDeclarationListImpl.covertDartClassPropertyModel() : DartClassProperty{
+fun FreezedCovertModel.getPropertiesString(): String {
+    val sb = StringBuilder()
+    if (properties.isNotEmpty()) {
+        properties.forEach {
+            sb.append(if (it.isNonNull) "       ${it.type} ${it.name}," else "      required ${it.type} ${it.name},")
+            sb.append("\n")
+        }
+    }
+    return sb.toString()
+}
+
+fun DartVarDeclarationListImpl.covertDartClassPropertyModel(): DartClassProperty {
     return DartClassProperty(
         type = DartPsiElementUtil.getTypeWithVar(this),
         name = DartPsiElementUtil.getNameWithVar(this),
@@ -11,5 +22,5 @@ fun DartVarDeclarationListImpl.covertDartClassPropertyModel() : DartClassPropert
     )
 }
 
-data class DartClassProperty(val type: String, val name: String,val isNonNull: Boolean)
+data class DartClassProperty(val type: String, val name: String, val isNonNull: Boolean)
 data class FreezedCovertModel(val properties: List<DartClassProperty>, val className: String)
