9394cd79e8b579b8aa03cdd35322a13a2938b440
梁典典 <413153189@qq.com>
2023-02-02 11:22:55 +0800

    :rocket: 新增模型转freezed工具

--- src/main/kotlin/shop/itbug/fluttercheckversionx/actions/FxModelToFreezedModel.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/actions/FxModelToFreezedModel.kt
@@ -3,7 +3,7 @@ package shop.itbug.fluttercheckversionx.actions
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import shop.itbug.fluttercheckversionx.dialog.FreezedCovertDialog
+import shop.itbug.fluttercheckversionx.dialog.FreezedClassesGenerateDialog
 import shop.itbug.fluttercheckversionx.services.impl.ModelToFreezedModelServiceImpl
 import shop.itbug.fluttercheckversionx.util.DartPsiElementUtil
 import shop.itbug.fluttercheckversionx.util.getDartClassDefinition
@@ -16,7 +16,8 @@ class FxModelToFreezedModel : AnAction() {
     private val toFreezedService = ModelToFreezedModelServiceImpl()
     override fun actionPerformed(e: AnActionEvent) {
         val model = toFreezedService.anActionEventToFreezedCovertModel(e)
-        FreezedCovertDialog(e.project!!, model).show()
+//        FreezedCovertDialog(e.project!!, model).show()
+        FreezedClassesGenerateDialog(e.project!!, mutableListOf(model))
     }
 
     override fun update(e: AnActionEvent) {
