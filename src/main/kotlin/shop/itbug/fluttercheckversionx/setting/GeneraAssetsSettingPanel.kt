f1372b404470107abf74ada642ea68ed487620f2
梁典典 <413153189@qq.com>
2023-02-12 21:26:41 +0800

    :memo:  修复通知栏的在多个编辑器显示的问题

--- src/main/kotlin/shop/itbug/fluttercheckversionx/setting/GeneraAssetsSettingPanel.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/setting/GeneraAssetsSettingPanel.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.ui.components.JBList
 import com.intellij.util.ui.components.BorderLayoutPanel
 import shop.itbug.fluttercheckversionx.icons.MyIcons
-import shop.itbug.fluttercheckversionx.widget.WidgetUtil
+import shop.itbug.fluttercheckversionx.widget.askString
 import java.awt.BorderLayout
 import javax.swing.DefaultListCellRenderer
 import javax.swing.DefaultListModel
@@ -13,39 +13,33 @@ import javax.swing.DefaultListModel
 /**
  * 生成资产文件的设置
  */
-class GeneraAssetsSettingPanel: BorderLayoutPanel() {
+class GeneraAssetsSettingPanel : BorderLayoutPanel() {
 
 
     private val igFilesWidget = IgFileList()
+
     init {
         addToTop(createToolBar())
         igFilesWidget.addItemString("test.png")
-        add(igFilesWidget,BorderLayout.CENTER)
+        add(igFilesWidget, BorderLayout.CENTER)
     }
 
     private fun createToolBar() = BorderLayoutPanel().apply {
         val actionGroup = DefaultActionGroup(*createActions())
-        val toolbar = ActionManager.getInstance().createActionToolbar("GenerateAssetsIgFileToolbar",actionGroup,true).apply {
-            setReservePlaceAutoPopupIcon(true)
-            layoutPolicy = ActionToolbar.NOWRAP_LAYOUT_POLICY
-            targetComponent = igFilesWidget
-        }
+        val toolbar =
+            ActionManager.getInstance().createActionToolbar("GenerateAssetsIgFileToolbar", actionGroup, true).apply {
+                setReservePlaceAutoPopupIcon(true)
+                layoutPolicy = ActionToolbar.NOWRAP_LAYOUT_POLICY
+                targetComponent = igFilesWidget
+            }
         addToLeft(toolbar.component)
     }
 
     private fun createActions(): Array<AnAction> = arrayOf(
-        object : DumbAwareAction(MyIcons.add){
+        object : DumbAwareAction(MyIcons.add) {
             override fun actionPerformed(e: AnActionEvent) {
-                addIgFileName()
+                e.project?.askString(this@GeneraAssetsSettingPanel.igFilesWidget::addItemString)
             }
-
-            private fun addIgFileName() {
-                WidgetUtil.getTextEditorPopup("请输入忽略的文件名,或者后缀","例如: *.json",{it.showInCenterOf(igFilesWidget)}){
-                    this@GeneraAssetsSettingPanel.igFilesWidget.addItemString(it)
-                }
-            }
-
-
         }
     )
 }
@@ -53,7 +47,7 @@ class GeneraAssetsSettingPanel: BorderLayoutPanel() {
 /**
  * 忽略的文件或者后缀
  */
-class IgFileList: JBList<String>() {
+class IgFileList : JBList<String>() {
 
     init {
         cellRenderer = DefaultListCellRenderer()
@@ -64,8 +58,6 @@ class IgFileList: JBList<String>() {
     fun addItemString(name: String) {
         (model as DefaultListModel).addElement(name)
     }
-
-
 
 
 }
