d81aec0835c63320831fabc2ab71acd9d950826c
梁典典 <413153189@qq.com>
2023-12-19 14:04:16 +0800

    重构资源生成器

---
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/services/AssetsListeningProjectService.kt
@@ -0,0 +1,175 @@
+package shop.itbug.fluttercheckversionx.services
+
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import com.intellij.util.messages.MessageBusConnection
+import io.flutter.sdk.FlutterSdk
+import shop.itbug.fluttercheckversionx.config.GenerateAssetsClassConfig
+import shop.itbug.fluttercheckversionx.icons.MyIcons
+import shop.itbug.fluttercheckversionx.util.MyDartPsiElementUtil
+import shop.itbug.fluttercheckversionx.util.RunUtil
+import shop.itbug.fluttercheckversionx.util.projectClosed
+
+
+class MyAssetGenPostStart : ProjectActivity {
+    override suspend fun execute(project: Project) {
+        AssetsListeningProjectService.getInstance(project).initListening()
+    }
+
+}
+
+class MyProjectListening : ProjectManagerListener {
+
+    override fun projectClosing(project: Project) {
+        AssetsListeningProjectService.getInstance(project).dispose()
+        super.projectClosing(project)
+    }
+}
+
+@Service(Service.Level.PROJECT)
+public final class AssetsListeningProjectService(val project: Project) {
+
+
+    private lateinit var connect: MessageBusConnection
+
+    private var checkFlutterVersionTask: Task = CheckFlutterVersionTask()
+
+
+    companion object {
+        fun getInstance(project: Project): AssetsListeningProjectService {
+            return project.getService(AssetsListeningProjectService::class.java)
+        }
+    }
+
+
+    ///销毁
+    fun dispose() {
+        project.projectClosed {
+            checkFlutterVersionTask.onCancel()
+            connect.dispose()
+            println("监听资源被释放")
+        }
+    }
+
+    ///初始化
+    fun initListening() {
+        println("进来了.")
+        ProgressManager.getInstance().run(checkFlutterVersionTask)
+        checkAssetsChange()
+    }
+
+
+    private fun checkAssetsChange() {
+        connect = project.messageBus.connect()
+        connect.subscribe(VirtualFileManager.VFS_CHANGES, object :
+            BulkFileListener {
+            override fun after(events: MutableList<out VFileEvent>) {
+                super.after(events)
+                if (project.isDisposed) {
+                    return
+                }
+                val projectPath = project.basePath
+                val setting = GenerateAssetsClassConfig.getGenerateAssetsSetting()
+                if (!setting.autoListenFileChange) {
+                    return
+                }
+                if (projectPath != null) {
+                    events.forEach {
+                        it.file?.apply {
+                            checkAndAutoGenFile(projectPath, this, project)
+                        }
+                    }
+                }
+
+
+            }
+
+        })
+    }
+
+
+    private fun checkAndAutoGenFile(projectPath: String, file: VirtualFile, project: Project) {
+        var filePath = file.canonicalPath
+        filePath = filePath?.replace("$projectPath/", "")
+        if (filePath != null) {
+            if (filePath.indexOf("assets") == 0) {
+                MyDartPsiElementUtil.autoGenerateAssetsDartClassFile(project, "assets", true)
+            }
+        }
+    }
+
+    ///检测flutter新版本弹出
+    private inner class CheckFlutterVersionTask : Task.Backgroundable(project, "Detecting Flutter version...") {
+        override fun run(indicator: ProgressIndicator) {
+            val currentFlutterVersion = FlutterSdk.getFlutterSdk(project)?.version
+            currentFlutterVersion?.let {
+                val version = FlutterService.getVersion()
+                version?.apply {
+                    val release = releases.find { o -> o.hash == currentRelease.stable }
+                    release?.let { r ->
+                        if (r.version != it.versionText) {
+                            println("has new version")
+                            showTip(r, project)
+                        }
+                    }
+                }
+            }
+        }
+
+        /**
+         * 弹出通知
+         */
+        fun showTip(release: Release, project: Project) {
+            val createNotification =
+                NotificationGroupManager.getInstance().getNotificationGroup("flutter_version_check").createNotification(
+                    "The new Flutter version is ready",
+                    "Update to the latest version: ${release.version}  (dart:${release.dartSDKVersion})",
+                    NotificationType.INFORMATION,
+                )
+            createNotification.setIcon(MyIcons.flutter)
+
+
+
+            createNotification.addAction(
+                object : AnAction("Upgrade") {
+                    override fun actionPerformed(p0: AnActionEvent) {
+                        RunUtil.runCommand(project, "flutter upgrade", "flutter upgrade")
+                        createNotification.hideBalloon()
+                    }
+
+                    override fun update(e: AnActionEvent) {
+                        e.presentation.icon = MyIcons.flutter
+                        super.update(e)
+                    }
+
+                    override fun getActionUpdateThread(): ActionUpdateThread {
+                        return ActionUpdateThread.BGT
+                    }
+
+                },
+            )
+            createNotification.addAction(object : AnAction("Cancel") {
+                override fun actionPerformed(p0: AnActionEvent) {
+                    createNotification.hideBalloon()
+
+                }
+            })
+            createNotification.notify(project)
+        }
+    }
+
+}
