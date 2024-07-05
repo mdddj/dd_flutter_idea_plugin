b0248675ac4a7e4c9021f9cedc673939be028dc4
梁典典 <413153189@qq.com>
2024-07-05 16:36:58 +0800

    修复版本自动修复功能

--- src/main/kotlin/shop/itbug/fluttercheckversionx/hints/PluginVersionHints.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/hints/PluginVersionHints.kt
@@ -4,6 +4,10 @@ import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.application.ex.ApplicationUtil
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import shop.itbug.fluttercheckversionx.i18n.PluginBundle
 import shop.itbug.fluttercheckversionx.services.PubService
 
 /// 自动加载版本号
@@ -12,14 +16,23 @@ class PluginVersionHints : CompletionContributor() {
     //    填充完成事件
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
         val currLinePluginName = parameters.originalPosition?.parent?.parent?.firstChild?.text
-        if (currLinePluginName != null) {
-            try {
-                val data = PubService.getPackageVersions(currLinePluginName)
-                data?.versions?.forEach {
-                    result.addElement(LookupElementBuilder.create(it))
+        val project = parameters.originalPosition?.project
+        if (currLinePluginName != null && project != null) {
+            val task = object : Task.Backgroundable(
+                project,
+                PluginBundle.get("fill.completion.task.title") + ":${currLinePluginName}"
+            ) {
+                override fun run(indicator: ProgressIndicator) {
+                    val r = ApplicationUtil.runWithCheckCanceled(
+                        { PubService.getPackageVersions(currLinePluginName) },
+                        indicator
+                    )
+                    r?.versions?.forEach {
+                        result.addElement(LookupElementBuilder.create(it))
+                    }
                 }
-            } catch (_: Exception) {
             }
+            task.queue()
         }
         super.fillCompletionVariants(parameters, result)
     }
--- src/main/kotlin/shop/itbug/fluttercheckversionx/tools/DartPluginVersionCheck.kt
+++ src/main/kotlin/shop/itbug/fluttercheckversionx/tools/DartPluginVersionCheck.kt
@@ -12,7 +12,7 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiRecursiveElementWalkingVisitor
-import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -37,12 +37,18 @@ class DartPluginVersionCheck : ExternalAnnotator<DartPluginVersionCheck.Input, L
     DumbAware {
 
     data class Input(val file: PsiFile, val element: List<PackageInfo>)
-    data class PackageInfo(val element: PsiElement, val packageInfo: DartPluginVersionName)
+    data class PackageInfo(
+        val element: YAMLKeyValueImpl,
+        val packageInfo: DartPluginVersionName,
+        val textPsiElement: YAMLPlainTextImpl
+    )
+
     data class Problem(
         val textRange: TextRange,
         val model: PubVersionDataModel,
         val element: PsiElement,
-        val lastVersion: String
+        val lastVersion: String,
+        val textPsiElement: YAMLPlainTextImpl
     )
 
     override fun collectInformation(file: PsiFile): Input {
@@ -50,11 +56,14 @@ class DartPluginVersionCheck : ExternalAnnotator<DartPluginVersionCheck.Input, L
         file.originalElement.accept(object : PsiRecursiveElementWalkingVisitor() {
             override fun visitElement(element: PsiElement) {
                 val ext = YamlExtends(element)
-                val info = ext.getDartPluginNameAndVersion()
-                val igSetting = DartPluginIgnoreConfig.getInstance(file.project)
-                if (ext.isDartPluginElement() && igSetting.isIg(info?.name ?: "").not()) {
-                    if (info != null) {
-                        elements.add(PackageInfo(element, info))
+                val info: DartPluginVersionName? = ext.getDartPluginNameAndVersion()
+                if (info != null) {
+                    val igSetting = DartPluginIgnoreConfig.getInstance(file.project)
+                    if (igSetting.isIg(info.name).not() && !ext.isSpecifyVersion()) {
+                        val lastTextElement = PsiTreeUtil.findChildOfType(element, YAMLPlainTextImpl::class.java)
+                        if (lastTextElement != null) {
+                            elements.add(PackageInfo(element as YAMLKeyValueImpl, info, lastTextElement))
+                        }
                     }
                 }
                 super.visitElement(element)
@@ -65,31 +74,38 @@ class DartPluginVersionCheck : ExternalAnnotator<DartPluginVersionCheck.Input, L
 
     //执行长时间操作
     override fun doAnnotate(collectedInfo: Input?): List<Problem> {
+        if (collectedInfo == null) return emptyList()
         val arr = mutableListOf<Problem>()
-        collectedInfo?.let {
-            val infos: List<PubVersionDataModel?> = runBlocking {
-                val tasks = it.element.map { info ->
-                    val pluginName = info.packageInfo.name
-                    val r: Deferred<PubVersionDataModel?> = async {
-                        return@async PubService.callPluginDetails(pluginName)
-                    }
-                    return@map r
+        val infos: List<PubVersionDataModel?> = runBlocking {
+            val tasks = collectedInfo.element.map { info ->
+                val pluginName = info.packageInfo.name
+                val r: Deferred<PubVersionDataModel?> = async {
+                    return@async PubService.callPluginDetails(pluginName)
                 }
-                return@runBlocking tasks.awaitAll()
+                return@map r
             }
-            it.element.forEach { info ->
-                val packageName = info.packageInfo.name
-                val find: PubVersionDataModel? = infos.find { detail -> detail?.name == packageName }
-                find?.let { model ->
-                    run {
-                        val versionText = model.getLastVersionText(info.packageInfo)
-                        if (versionText != null) {
-                            arr.add(Problem(info.element.lastChild.textRange, model, info.element, versionText))  //有新版本
-                        }
-                    }
-                }
-            }
+            return@runBlocking tasks.awaitAll()
+        }
+        collectedInfo.element.forEach { info ->
+            val packageName = info.packageInfo.name
+            val find: PubVersionDataModel? = infos.find { detail -> detail?.name == packageName }
+            find?.let { model ->
+                run {
+                    val versionText = model.getLastVersionText(info.packageInfo)
 
+                    if (versionText != null) {
+                        arr.add(
+                            Problem(
+                                info.textPsiElement.textRange,
+                                model,
+                                info.element,
+                                versionText,
+                                info.textPsiElement
+                            )
+                        )  //有新版本
+                    }
+                }
+            }
         }
         return arr
     }
@@ -101,31 +117,28 @@ class DartPluginVersionCheck : ExternalAnnotator<DartPluginVersionCheck.Input, L
             holder.newAnnotation(
                 HighlightSeverity.WARNING, "${PluginBundle.get("version.tip.1")}:${it.lastVersion}"
             )
+                .tooltip(PluginBundle.get("dart.fix.version.time.tip.title") + it.model.lastVersionUpdateTimeString)
                 .newFix(object : PsiElementBaseIntentionAction(), Iconable, DumbAware {
                     override fun getFamilyName() = fixText
                     override fun getText() = fixText
-                    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
-                        return element.text != it.lastVersion
-                    }
+                    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean =
+                        element.text != it.lastVersion
 
                     override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
                         val newElement = MyYamlPsiElementFactory.createPlainPsiElement(project, it.lastVersion)
                         if (newElement != null) {
-                            if (element is LeafPsiElement && element.prevSibling is YAMLKeyValueImpl && element.prevSibling.lastChild is YAMLPlainTextImpl) {
-                                element.prevSibling.lastChild.replace(newElement)
-                            } else {
-                                element.replace(newElement)
-                            }
+                            it.textPsiElement.replace(newElement)
                         }
                     }
 
+
                     override fun getIcon(flags: Int): Icon = MyIcons.flutter
 
-                }).registerFix().range(it.element.lastChild)
-                .needsUpdateOnTyping()
+                }).registerFix().range(it.textRange)
                 .create()
 
         }
 
     }
+
 }
