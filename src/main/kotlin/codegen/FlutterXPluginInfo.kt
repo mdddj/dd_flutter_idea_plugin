36e8bba42309d2829c227c29b1b1de41e870efe2
梁典典 <413153189@qq.com>
2025-05-06 11:32:52 +0800

    添加日志

--- src/main/kotlin/codegen/FlutterXPluginInfo.kt
+++ src/main/kotlin/codegen/FlutterXPluginInfo.kt
@@ -1,11 +1,11 @@
 package codegen
 // 自动生成的插件信息类,不要修改这个文件,否则会导致插件功能失效
 object FlutterXPluginInfo {
-    const val VERSION: String = "5.6.0.243"
+    const val VERSION: String = "5.6.1.251"
     const val CHANGELOG: String = """
-<h2>5.6.0 - 2025-04-28</h2>
+<h2>5.6.1 - 2025-05-06</h2>
 
-<ul><li>Refactoring Log Panel, see <a href="https://flutterx.itbug.shop/log.html">document</a></li><li>Fix known bugs</li><li><a href="https://github.com/mdddj/dd_flutter_idea_plugin/blob/master/CHANGELOG.md">Full changelog</a></li></ul>
+<ul><li>Unused package logic detection optimization <a href="https://github.com/mdddj/dd_flutter_idea_plugin/issues/78">#78</a></li><li><a href="https://github.com/mdddj/dd_flutter_idea_plugin/blob/master/CHANGELOG.md">full changelog</a></li></ul>
 """
 
 }
